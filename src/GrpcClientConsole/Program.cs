c349d707efc2f836a1733e8f1fb60970a7a01cfe
Viwe Nkepu <viwe.nkepu@hotmail.com>
2022-11-16 11:49:36 +0200

    Add channel options

--- src/GrpcClientConsole/Program.cs
+++ src/GrpcClientConsole/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Grpc.Core;
 using Grpc.Net.Client;
+using Grpc.Net.Client.Configuration;
 using GrpcClientConsole.Protos;
 
 Console.WriteLine("Hello, World!");
@@ -8,7 +9,28 @@ Console.WriteLine("Hello, World!");
 try
 {
 	var data = new HeroRequest { HeroId = 13 };
-	var grpcChannel = GrpcChannel.ForAddress("https://localhost:7062");
+	var grpcChannel = GrpcChannel.ForAddress("https://localhost:7062", new GrpcChannelOptions()
+	{
+		ServiceConfig = new ServiceConfig()
+		{
+			MethodConfigs =
+			{
+				new MethodConfig()
+				{
+					Names = { MethodName.Default },
+					RetryPolicy= new RetryPolicy()
+					{
+						MaxAttempts = 5,
+						InitialBackoff= TimeSpan.FromSeconds(2),
+						MaxBackoff = TimeSpan.FromSeconds(5),
+						BackoffMultiplier = 1.5,
+						RetryableStatusCodes = { StatusCode.Unknown, StatusCode.Unavailable }
+					}
+				}
+			}
+
+		}
+	});
 	var client = new Hero.HeroClient(grpcChannel);
 	var response = await client.GetHeroByIDAsync(data);
 	Console.WriteLine(response);
